cdf792278af392aa6b57a5eb8ba6a711b0b52fea
XuanDatNguyen <nguyenxuandat.bqp@gmail.com>
2022-10-16 23:49:09 +0700

    add lib multer, folder utils

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+        cb(null, uniqueName)
+    },
+});
 
 router.post('/', (req, res) => {
     //validate request
     if (!req.file) {
-        return res.json({error: 'File is required!'})
+        return res.json({ error: 'File is required!' })
     }
 
     //store file
